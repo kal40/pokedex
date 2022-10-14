b099f188d75697a0ab77546b6b874956d1919690
kal40 <karoly.lonich@smartcoder.dev>
2022-10-14 13:10:13 +0200

    feat: add pokemon list
    Add an array of pokemon objects.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+let pokemonList = [
+  {
+    name: "Bulbasaur",
+    height: 0.7,
+    types: ["Monster", "Grass"],
+  },
+  {
+    name: "Charmander",
+    height: 0.6,
+    types: ["Monster", "Dragon"],
+  },
+  {
+    name: "Squirtle",
+    height: 0.5,
+    types: ["Monster", "Water 1"],
+  },
+];
