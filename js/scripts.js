4ec6acf1bacb92f3085d5a54e8904b767ec9894f
kal40 <karoly.lonich@smartcoder.dev>
2022-10-17 15:49:44 +0200

    refactor: implement IIFE for globa variables
    * Wrap pokemonList array in an IIFE to avoid accidentally accessing the global state
    * Add public functions to retrieve pokemonList and to add new pokemons to the list

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,37 @@
-let pokemonList = [
-  {
-    name: "Bulbasaur",
-    height: 0.7,
-    types: ["Monster", "Grass"],
-  },
-  {
-    name: "Charmander",
-    height: 0.6,
-    types: ["Monster", "Dragon"],
-  },
-  {
-    name: "Squirtle",
-    height: 0.5,
-    types: ["Monster", "Water 1"],
-  },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: "Bulbasaur",
+      height: 0.7,
+      types: ["Monster", "Grass"],
+    },
+    {
+      name: "Charmander",
+      height: 0.6,
+      types: ["Monster", "Dragon"],
+    },
+    {
+      name: "Squirtle",
+      height: 0.5,
+      types: ["Monster", "Water 1"],
+    },
+  ];
 
-pokemonList.forEach(function (item) {
+  let add = function (item) {
+    pokemonList.push(item);
+  };
+
+  let getAll = function () {
+    return pokemonList;
+  };
+
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
+
+pokemonRepository.getAll().forEach(function (item) {
   let displayInfo = `${item.name} (height: ${item.height})`;
   if (item.height > 0.6) {
     displayInfo += " - Wow, that’s big!";
